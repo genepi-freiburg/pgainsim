1860ab74915f6f7e10f3349167c15a53544cd0e7
Pascal Schlosser <pascal.schlosser@uniklinik-freiburg.de>
2021-01-19 19:23:43 +0100

    extended vignette

--- vignettes/pgainsim.R
+++ vignettes/pgainsim.R
@@ -2,20 +2,23 @@
 knitr::opts_chunk$set(error = TRUE)
 
 ## ----install_package----------------------------------------------------------
-devtools::install_github("genepi-freiburg/pgainsim")
+if (!requireNamespace("devtools", quietly = TRUE))
+   install.packages("devtools")
+if (!requireNamespace("pgainsim", quietly = TRUE))
+  devtools::install_github("genepi-freiburg/pgainsim")
 
 ## ----simulation---------------------------------------------------------------
-library(pgainsim)
-sim_dat <- p_gain_simulation(pgain_types=c("add","rec"),AFs=c(0.3,0.7),n=10000L, cores=4L)
+require(pgainsim)
+sim_dat <- p_gain_simulation(pgain_types=c("add","rec"), AFs=c(0.3,0.7), n=10000L, cores=4L)
 
 ## ----density_plot-------------------------------------------------------------
-p_gain_density_plot(pgain_type="rec", sim_data=sim_dat, xlim=c(0,5))
+p_gain_density_plot(pgain_type="rec", sim_data=sim_dat, xlim=c(0,5), print_pdf=FALSE)
 
 ## ----quantiles----------------------------------------------------------------
-quantile <- p_gain_quantiles(n_tests=5L,sim_data=sim_dat)
+quantile <- p_gain_quantiles(n_tests=5L, sim_data=sim_dat)
 
 ## ----quantiles_fit------------------------------------------------------------
-list_fits <- p_gain_quantile_fit(pgain_quantile=quantile,test_number=50L)
+list_fits <- p_gain_quantile_fit(pgain_quantile=quantile, test_number=50L, print_pdf=FALSE)
 
 ## ----sessionInfo--------------------------------------------------------------
 sessionInfo()
